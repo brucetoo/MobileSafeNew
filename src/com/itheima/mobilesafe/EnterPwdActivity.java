fcafaa8406d52bde32ce2977f350126d4c8d1076
bruce <676047577@qq.com>
2014-08-01 17:47:43 +0800

    程序锁

---
+++ src/com/itheima/mobilesafe/EnterPwdActivity.java
@@ -0,0 +1,18 @@
+package com.itheima.mobilesafe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Bruce
+ * Data 2014/8/1
+ * Time 16:40.
+ */
+public class EnterPwdActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_enter_pwd);
+    }
+}
--- src/com/itheima/mobilesafe/service/WatchDogService.java
+++ src/com/itheima/mobilesafe/service/WatchDogService.java
@@ -4,6 +4,8 @@ import android.app.ActivityManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import com.itheima.mobilesafe.EnterPwdActivity;
+import com.itheima.mobilesafe.db.dao.AppLockDao;
 
 import java.util.List;
 
@@ -16,8 +18,8 @@ import java.util.List;
  */
 public class WatchDogService extends Service{
 
-    boolean flag;
-
+    private boolean flag;
+    private AppLockDao dao;
     @Override
     public IBinder onBind(Intent intent) {
         return null;
@@ -26,6 +28,7 @@ public class WatchDogService extends Service{
     @Override
     public void onCreate() {
         final ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        dao = new AppLockDao(this);
         flag = true;
         new Thread() {
             @Override
@@ -36,7 +39,14 @@ public class WatchDogService extends Service{
                     //获取栈顶的activity程序
                     String packname = taskInfos.get(0).topActivity.getPackageName();
                     System.out.println("packname = " + packname);
-
+                    //packname 是表示当前真正运行的程序
+                    if(dao.findApp(packname)){ //如果 在数据库中，这锁该程序
+                       Intent intent = new Intent(getApplicationContext(), EnterPwdActivity.class);
+                        //服务没有任务栈，在开启Activity时需要指定一个
+                       intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                    //每隔 50mm  取一次
                     try {
                         Thread.sleep(50);
                     } catch (InterruptedException e) {
