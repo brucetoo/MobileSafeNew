1f5d139c76483fa10a06b43f8f0e65aea81d117a
bruce <676047577@qq.com>
2014-08-04 12:01:26 +0800

    1.WatchDogService 看门狗的服务主健之间的通讯，可以用自定义广播其中定义了自定义广播的接收和关屏广播的接收服务中启动 activity时 没有任务栈，在开启Activity时需要指定一个2.EnterPwdActivity重写了 onBackPress方法自定义广播和WatchDogService服务进行通讯

--- src/com/itheima/mobilesafe/EnterPwdActivity.java
+++ src/com/itheima/mobilesafe/EnterPwdActivity.java
@@ -1,7 +1,16 @@
 package com.itheima.mobilesafe;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Bruce
@@ -9,10 +18,78 @@ import android.os.Bundle;
  * Time 16:40.
  */
 public class EnterPwdActivity extends Activity {
+    private EditText et_password;
+    private TextView tv_name;
+    private ImageView iv_icon;
+    private String packname; //应用程序的包名
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_enter_pwd);
+
+        Intent intent = getIntent();
+        //得到被锁定应用程序的包名
+        packname = intent.getStringExtra("packname");
+        et_password = (EditText) findViewById(R.id.et_password);
+
+        tv_name = (TextView) findViewById(R.id.tv_name);
+        iv_icon = (ImageView) findViewById(R.id.iv_icon);
+        PackageManager pm = getPackageManager();
+        try {
+            ApplicationInfo  info = pm.getApplicationInfo(packname, 0);
+            tv_name.setText(info.loadLabel(pm));
+            iv_icon.setImageDrawable(info.loadIcon(pm));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+       // super.onBackPressed();
+    /*    <action android:name="android.intent.action.MAIN" />
+        <category android:name="android.intent.category.DEFAULT" />
+        <category android:name="android.intent.category.HOME" />
+        <category android:name="android.intent.category.MONKEY" />*/
+
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.MAIN");
+        intent.addCategory("android.intent.category.DEFAULT");
+        intent.addCategory("android.intent.category.HOME");
+        intent.addCategory("android.intent.category.MONKEY");
+        startActivity(intent);
+
+        /**
+         * 所有Activity的最小化操作都不会执行 onDestroy方法，只执行 onStop 方法
+         * 在按返回键  返回桌面 实际上是在重新 Home的方法，因此执行完后必须是finish()该activity
+         */
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        finish();
+    }
+
+    public void Confirm(View view){
+      String pass = et_password.getText().toString().trim();
+        if(TextUtils.isEmpty(pass)){
+            Toast.makeText(this,"please input password!",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(pass.equals("123")){
+            //在关闭activity之前，需要通知看门狗 临时停止保护程序
+            //自定义广播，完成组建中的通讯
+            Intent intent = new Intent();
+            intent.setAction("com.bruce.temstop");
+            intent.putExtra("packname",packname);  //发送需要解锁的包名
+            sendBroadcast(intent);
+            finish();
+        }else {
+            Toast.makeText(this,"wrong password!",Toast.LENGTH_LONG).show();
+        }
+
     }
 }
--- src/com/itheima/mobilesafe/service/WatchDogService.java
+++ src/com/itheima/mobilesafe/service/WatchDogService.java
@@ -2,7 +2,10 @@ package com.itheima.mobilesafe.service;
 
 import android.app.ActivityManager;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import com.itheima.mobilesafe.EnterPwdActivity;
 import com.itheima.mobilesafe.db.dao.AppLockDao;
@@ -16,17 +19,50 @@ import java.util.List;
  * <p/>
  * 程序开启的 看门狗
  */
-public class WatchDogService extends Service{
+public class WatchDogService extends Service {
 
-    private boolean flag;
-    private AppLockDao dao;
+    private boolean flag;      //在服务停止的时候终止
+    private AppLockDao dao;    //数据库操作
+    private InnerReceiver reciever;  //接收自定义的广播
+    private String tempStopPackname;//临时需要解锁的包名
+
+    private ScreenReceiver screenReceiver;
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
+
+    //接收自定义的广播
+    private class InnerReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //接收到了临时停止保护的广播事件
+            // System.out.println("接收到了临时停止保护的广播事件");
+            tempStopPackname = intent.getStringExtra("packname");
+        }
+    }
+
+    //接收锁屏的广播
+    private class ScreenReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //锁屏就清空
+            tempStopPackname = null;
+        }
+    }
+
     @Override
     public void onCreate() {
+
+        reciever = new InnerReceiver();
+        registerReceiver(reciever, new IntentFilter("com.bruce.temstop"));
+
+        screenReceiver = new ScreenReceiver();
+        registerReceiver(screenReceiver, new IntentFilter(Intent.ACTION_SCREEN_OFF)); //注册关屏的广播
+
         final ActivityManager am = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
         dao = new AppLockDao(this);
         flag = true;
@@ -40,11 +76,16 @@ public class WatchDogService extends Service{
                     String packname = taskInfos.get(0).topActivity.getPackageName();
                     System.out.println("packname = " + packname);
                     //packname 是表示当前真正运行的程序
-                    if(dao.findApp(packname)){ //如果 在数据库中，这锁该程序
-                       Intent intent = new Intent(getApplicationContext(), EnterPwdActivity.class);
-                        //服务没有任务栈，在开启Activity时需要指定一个
-                       intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intent);
+                    if (dao.findApp(packname)) { //如果 在数据库中，这锁该程序
+                        //广播中需要停止保护的程序包
+                        if (!packname.equals(tempStopPackname)) { //不存在，才去保护
+                            Intent intent = new Intent(getApplicationContext(), EnterPwdActivity.class);
+                            //设置要保护程序的包名
+                            intent.putExtra("packname", packname);
+                            //服务没有任务栈，在开启Activity时需要指定一个
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        }
                     }
                     //每隔 50mm  取一次
                     try {
@@ -63,6 +104,10 @@ public class WatchDogService extends Service{
     @Override
     public void onDestroy() {
         flag = false;
+        unregisterReceiver(reciever);
+        reciever = null;
+        unregisterReceiver(screenReceiver);
+        screenReceiver = null;
         super.onDestroy();
     }
 }
